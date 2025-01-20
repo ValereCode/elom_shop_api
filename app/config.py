e7619c52a8d267b070973e932751e6383705eb4b
ValereCode <setodji2001@gmail.com>
2025-01-20 12:58:24 +0000

    load_env

--- app/config.py
+++ app/config.py
@@ -1,5 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 class Settings(BaseSettings):
     database_url: str = os.getenv("DATABASE_URL")
